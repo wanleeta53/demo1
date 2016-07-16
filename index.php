25d075f650ed6daa5f086b0f6072318351837e0a
wanlee <wanlee@atcreative.co.th>
2016-07-16 11:51:05 +0700

    add array

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 echo "Today is " . date("Y/m/d") . "<br>";
+
+$cars = array("Volvo", "BMW", "Toyota");
 echo "I like 1 ==> " . $cars[0]. "<br>";
 echo "I like 2 ==> " . $cars[1]. "<br>";
+echo "I like 3 ==> " . $cars[2];
+
 ?>
